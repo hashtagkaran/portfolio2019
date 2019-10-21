fec2fa5a558c4cdc9a8a67db9b93ff943ce7c013
karan singh <thekrsingh@gmail.com>
2019-10-21 17:14:24 +0530

    v1.2

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function projects() {
     });
 };
 
+function learnMore() {
+    document.querySelector("#box2").scrollIntoView({
+        behavior: "smooth"
+    });
+};
+
+function skills() {
+    document.querySelector("#box4").scrollIntoView({
+        behavior: "smooth"
+    });
+};
+
